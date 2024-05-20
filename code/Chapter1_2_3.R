4bff1fdb9f965ba9e8439456385bc6361610426c
Leonhard Reiter <117082106+leonhardreitr@users.noreply.github.com>
2024-05-20 17:23:40 +0200

    Basics

--- code/Chapter1_2_3.R
+++ code/Chapter1_2_3.R
@@ -3,6 +3,8 @@
 
 # packages
 library(tidyverse)
+library(easystats)
+library(tidymodels)
 theme_set(theme_bw())
 
 # models need to be easy to use and encourage good methodological practice
@@ -33,3 +35,20 @@ ggplot(
   ) +
   labs(x = "Temperature (C)", y = "Chirp Rate (per minute)")
 
+inter_fit <- lm(rate ~ temp * species, data = crickets)
+no_inter <- lm(rate ~ temp + species, data = crickets)
+
+compare_performance(inter_fit,no_inter)
+
+
+# okay reading further I suppose that the goal of tidymodels is to standardize R modelling approaches, which is nice
+
+corr_res <- map(mtcars %>% select(-mpg), cor.test, y = mtcars$mpg)
+
+corr_res %>% 
+  # Convert each to a tidy format; `map_dfr()` stacks the data frames 
+  map_dfr(tidy, .id = "predictor") %>% 
+  ggplot(aes(x = fct_reorder(predictor, estimate))) + 
+  geom_point(aes(y = estimate)) + 
+  geom_errorbar(aes(ymin = conf.low, ymax = conf.high), width = .1) +
+  labs(x = NULL, y = "Correlation with mpg")
