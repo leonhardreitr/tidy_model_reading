c26ccc029392b0350c306a12c2796d4129572171
Leonhard Reiter <117082106+leonhardreitr@users.noreply.github.com>
2024-05-26 17:57:51 +0200

    Bipbop

--- code/Modeling_basics.R
+++ code/Modeling_basics.R
@@ -6,10 +6,8 @@ library(tidymodels)
 library(patchwork)
 
 tidymodels_prefer()
-theme_set(theme_bw() +
-            theme(
-              plot.title.position = "plot"
-            ))
+theme_set(theme_gray(base_size = 12) +
+            theme(panel.grid = element_blank()))
 # Housing Data
 
 # Lets go -----------------------------------------------------------------
@@ -44,6 +42,7 @@ ames |>
   ggtitle("Log")
 p1 / p2
 
+
 ames$Sale_Price <- log10(ames$Sale_Price)
 
 
@@ -54,4 +53,44 @@ set.seed(502)
 ames_split <- initial_split(ames, prop = 0.80, strata = Sale_Price)
 ames_train <- training(ames_split)
 ames_test  <-  testing(ames_split)
+
+
+# okay i now know how to create a training and test split
+# yippie
+
+
+  
+# Fitting Models ----------------------------------------------------------
+library(tidymodels)
+data(ames)
+ames <- mutate(ames, Sale_Price = log10(Sale_Price))
+
+set.seed(502)
+ames_split <- initial_split(ames, prop = 0.80, strata = Sale_Price)
+ames_train <- training(ames_split)
+ames_test  <-  testing(ames_split)
+
+lm_model <- linear_reg() %>% set_engine("lm")
+
+
+# 7. A Model Workflow -----------------------------------------------------
+
+library(tidymodels)
+data(ames)
+
+ames <- mutate(ames, Sale_Price = log10(Sale_Price))
+
+set.seed(502)
+ames_split <- initial_split(ames, prop = 0.80, strata = Sale_Price)
+ames_train <- training(ames_split)
+ames_test  <-  testing(ames_split)
+
+lm_model <- linear_reg() %>% set_engine("lm")
+
+lm_wflow <- 
+  workflow() %>% 
+  add_model(lm_model) %>% 
+  add_variables(outcome = Sale_Price, predictors = c(Longitude, Latitude))
+
+lm_fit <- fit(lm_wflow, ames_train)
 
